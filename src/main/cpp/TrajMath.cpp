cdbeda20b4dec02b640359936938d6bbeec88242
Brad Collins <bsc@housecollins.com>
2024-02-26 21:44:31 -0600

    Converted GetFiringAngle to equation-as-code
    Made the code look a little closer to its reference equation.
    
    Also added a comment with the reference equation and it source.

--- src/main/cpp/TrajMath.cpp
+++ src/main/cpp/TrajMath.cpp
@@ -2,25 +2,33 @@
 
 double t34::TrajMath::GetFiringAngle()
 {
-    return 
-    (
-        _180_DIV_PI *
-        atan(
-            (pow(m_note_max_velocity_mps, 2) - 
-            sqrt(
-                pow(m_note_max_velocity_mps, 4) - 9.807 * 
-                (
-                    (9.807 * pow(m_target_distance_meters, 2)) + (2 * pow(m_note_max_velocity_mps, 2) * m_target_height_meters)
-                )
-            )) / (9.807 * m_target_distance_meters)
-        )
-    );
+    //                   ____________________
+    //          ⎛  v² - √ v⁴ - g(gx² + 2v²y)   ⎞
+    // θ = atan ⎜ ———————————————————————————— ⎟
+    //          ⎝               gx             ⎠
+    //
+    // Source:
+    //   Solving Ballistic Trajectories <https://www.forrestthewoods.com/blog/solving_ballistic_trajectories/>
+    //   See section “Firing Angle to Hit Stationary Target.”
+
+    const auto v² = m_note_max_velocity_mps * m_note_max_velocity_mps;
+    const auto v⁴ = v² * v²;
+    const auto x = m_target_distance_meters;
+    const auto x² = x * x;
+    const auto y = m_target_height_meters;
+    const auto g = 9.80665; // gravity
+
+    const auto numerator = v² - sqrt(v⁴ - (g * ((g * x²) + (2  * v² * y))));
+    const auto denominator = g * x;
+    const auto θ = atan(numerator / denominator);
+
+    return RAD_TO_DEG(θ);
 }
 
 bool t34::TrajMath::IsInRange()
 {
     double rate = (
-        tan(m_shooter_angle) - 
+        tan(m_shooter_angle) -
         (9.807 * m_target_distance_meters) / (pow(m_shooter_angle, 2) * pow(m_note_max_velocity_mps, 2))
     );
 
@@ -36,9 +44,9 @@ bool t34::TrajMath::IsInRange()
 
 double t34::TrajMath::GetDistanceFromTarget()
 {
-    return ( 
+    return (
         (m_target_height_meters - m_limelight_height_meters) / tan(
-            RAD_TO_DEG( (m_limelight_angle + m_target_ty) ) 
+            RAD_TO_DEG( (m_limelight_angle + m_target_ty) )
         )
     );
 }
