85970391403792ba442b1c7da8e919452066422a
LoganR_FRC_Team34 <loganrilling@dpi.lcsk12.org>
2024-02-27 20:05:30 -0600

    WIP

--- src/main/include/TrajMath.h
+++ src/main/include/TrajMath.h
@@ -18,16 +18,8 @@ namespace t34
         double m_target_tx;
         double m_target_ty;
 
-        const double v2 = m_note_max_velocity_mps * m_note_max_velocity_mps;
-        const double v4 = v2 * v2;
-        const double x = m_target_distance_meters;
-        const double x2 = x * x;
-        const double y = m_target_height_meters;
         const double g = 9.80665; // gravity
 
-        const double gx2 = g * x2;
-        const double v2y = v2 * y;
-        const double gx = g * x;
 
     public:
 
