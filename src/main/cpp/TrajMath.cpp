63a9107304bf7eb350dec07dfc6553ca96ca7616
Brad Collins <bsc@housecollins.com>
2024-02-28 08:29:05 -0600

    Tweaked some variable names

--- src/main/cpp/TrajMath.cpp
+++ src/main/cpp/TrajMath.cpp
@@ -19,9 +19,28 @@ t34::TrajMath::TrajMath
 
 double t34::TrajMath::GetFiringAngleDeg()
 {
-    const double numerator = v2 - sqrt(v4 - (g * (gx2 + (2  * v2y))));
-    const double denominator = gx;
-    const double θ = atan(numerator / denominator);
+    //                   ____________________
+    //          ⎛  v² - √ v⁴ - g(gx² + 2v²y)   ⎞
+    // θ = atan ⎜ ———————————————————————————— ⎟
+    //          ⎝               gx             ⎠
+    //
+    // Source:
+    //   Solving Ballistic Trajectories <https://www.forrestthewoods.com/blog/solving_ballistic_trajectories/>
+    //   See section “Firing Angle to Hit Stationary Target.”
+
+    const auto v2 = m_note_max_velocity_mps * m_note_max_velocity_mps;
+    const auto v4 = v2 * v2;
+    const auto x  = m_target_distance_meters;
+    const auto x2 = x * x;
+    const auto y  = m_target_height_meters;
+
+    const auto gx  = g * x;
+    const auto gx2 = g * x2;
+    const auto v2y = v2 * y;
+
+    const auto numerator = v2 - sqrt(v4 - (g * (gx2 + (2*v2y))));
+    const auto denominator = gx;
+    const auto θ = atan(numerator / denominator);
 
     return RAD_TO_DEG(θ);
 }
