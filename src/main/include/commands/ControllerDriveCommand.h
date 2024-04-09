2045b18cb0c5ca67fd74da33d58d030c2f31d262
Brad Collins <bsc@housecollins.com>
2024-04-09 17:48:38 -0500

    Joystick slew rate limiter

--- src/main/include/commands/ControllerDriveCommand.h
+++ src/main/include/commands/ControllerDriveCommand.h
@@ -9,6 +9,7 @@
 
 #include "subsystems/SwerveDrive.h"
 #include "T34Controller.hpp"
+#include <frc/filter/SlewRateLimiter.h>
 
 namespace t34 {
 
@@ -24,6 +25,7 @@ namespace t34 {
         void Execute() override;
         void End(bool interrupted) override;
         bool IsFinished() override;
+        void JoystickSlewRateLimiter();
 
     private:
         std::shared_ptr<SwerveDrive> m_swerve_drive;
@@ -31,6 +33,10 @@ namespace t34 {
         double m_driving_speed;
 
         std::chrono::time_point<std::chrono::steady_clock> m_last_zero;
+
+        frc::SlewRateLimiter<units::scalar> m_x_speed_limiter{0.2 / 1_s};
+        frc::SlewRateLimiter<units::scalar> m_y_speed_limiter{0.2 / 1_s};
+        frc::SlewRateLimiter<units::scalar> m_rot_speed_limiter{0.2 / 1_s};
     };
 
 }
